6e7634fed0ab8073d1b0f7ec817d2a35552fc611
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-01-10 02:20:55 -0800

    slider for requests

--- src/routes/Requests/index.js
+++ src/routes/Requests/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 import RequestCard from "../../components/card/request"
+import { Tabs } from 'antd';
 import "../index.css"
 
 import SAMPLE_REQUESTS from "../../constants/sampleRequests"
 
+const { TabPane } = Tabs;
+
 const Requests = () => {
+
+    const completedRequests = [];
+    const incompletedRequests = [];
+
+    SAMPLE_REQUESTS.forEach((request) => {
+        if(new Date(request.completionDate) <= new Date()) completedRequests.push(request)
+        else incompletedRequests.push(request)
+    })
 
     return (
         <div className="page-background">
@@ -14,7 +25,14 @@ const Requests = () => {
             </div>
             <div className="content-background">
                 <div className="content">
-                    {SAMPLE_REQUESTS.map((request) => <RequestCard {...request} /> )}
+                <Tabs defaultActiveKey="1" centered>
+                    <TabPane tab="Current" key="1">
+                        {incompletedRequests.map((request) => <RequestCard {...request} /> )}
+                    </TabPane>
+                    <TabPane tab="Past" key="2">
+                        {completedRequests.map((request) => <RequestCard {...request} /> )}
+                    </TabPane>
+                </Tabs>
                 </div>
             </div>
         </div>
